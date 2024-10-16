20a9ac43551f72e5f0a61197ec72262ed3506b0c
milanesadeternera <facundojcolombo@gmail.com>
2024-10-16 10:38:43 -0300

    commit

---
+++ init.js
@@ -0,0 +1,8 @@
+function showOptions(){
+    let menu = document.getElementById("options");
+    if(menu.style.display == "none"||menu.style.display ==""){
+        menu.style.display = "block";
+    }else{
+        menu.style.display = "none"
+    }
+}
